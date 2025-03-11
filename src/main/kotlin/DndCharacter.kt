aaa50662854ca82d54cfb3a9b5670b07ec34b916
Oleh Korenkov <Oleh_Korenkov@epam.com>
2025-03-11 19:34:54 +1100

    Changes

--- src/main/kotlin/DndCharacter.kt
+++ src/main/kotlin/DndCharacter.kt
@@ -3,25 +3,21 @@ import kotlin.math.floor
 
 class DndCharacter {
 
-    val strength: Int by lazy { ability() }
-    val dexterity: Int by lazy { ability() }
-    val constitution: Int by lazy { ability() }
-    val intelligence: Int by lazy { ability() }
-    val wisdom: Int by lazy { ability() }
-    val charisma: Int by lazy { ability() }
+    val strength: Int by lazy(::ability)
+    val dexterity: Int by lazy(::ability)
+    val constitution: Int by lazy(::ability)
+    val intelligence: Int by lazy(::ability)
+    val wisdom: Int by lazy(::ability)
+    val charisma: Int by lazy(::ability)
     val hitpoints: Int by lazy { 10 + modifier(constitution) }
 
     companion object {
 
-        fun ability(): Int {
-            val rolls = List(4) { Random.nextInt(1, 7) } 
-            return rolls.sortedDescending().take(3).sum() 
-        }
+        fun ability(): Int = List(4) { Random.nextInt(1, 7) }
+            .sortedDescending()
+            .take(3)
+            .sum()
 
-        fun modifier(score: Int): Int {
-            return floor((score - 10) / 2.0).toInt() 
-        }
+        fun modifier(score: Int): Int = floor((score - 10) / 2.0).toInt()
     }
-
-    
 }
